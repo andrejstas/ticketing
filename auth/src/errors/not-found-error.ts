ca3a62f86880426cfd66df1a13e555a462691e61
Andrej Staš <andrej.stas.net@gmail.com>
2021-04-18 20:38:24 +0200

    Handle not found API endpoints.

---
+++ auth/src/errors/not-found-error.ts
@@ -0,0 +1,18 @@
+import { CustomError } from "./custom-error";
+
+export class NotFoundError extends CustomError {
+  statusCode = 404;
+  constructor() {
+    super("Not found file");
+
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeErrors(): { message: string; field?: string }[] {
+    return [
+      {
+        message: "Not found",
+      },
+    ];
+  }
+}
