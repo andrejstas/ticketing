91ba190a3c264eccc84f2354c16de13c3df66d54
Andrej Staš <andrej.stas.net@gmail.com>
2021-04-18 15:09:54 +0200

    Create routes for auth.

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post("/api/users/signup", (req: Request, res: Response) => {});
+
+export { router as signupRouter };
