e7ab3ce3385cabcdafd93585458a838b9638f4c8
Andrej Staš <andrej.stas.net@gmail.com>
2021-04-23 14:18:49 +0200

    Create a new user.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4, max: 30 })
       .withMessage("Password must be between 4 to 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -22,10 +22,17 @@ router.post(
     }
 
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
 
-    throw new DatabaseConnectionError();
+    if (existingUser) {
+      console.log("Email in use");
+      return res.send({});
+    }
 
-    res.send({});
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
