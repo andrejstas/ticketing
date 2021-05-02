1e3a511c05eb43d1c4003c15bc0b5e9939115c7c
Andrej Staš <andrej.stas.net@gmail.com>
2021-05-02 16:16:09 +0200

    Add more sign up tests.

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,52 @@ it("returns a 201 on successful sign up", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@testom",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@testom",
+      password: "",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "me@me.com" })
+    .expect(400);
+  await request(app)
+    .post("/api/users/signup")
+    .send({ password: "123123" })
+    .expect(400);
+});
+
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
